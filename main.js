3f16ee74ee528675841ca65701fef308af69735d
Rhys Perry <rhysperry111@gmail.com>
2019-04-17 18:21:00 +0100

    Made easier to read

--- main.js
+++ main.js
@@ -9,12 +9,12 @@ var xunit;
 var yunit;
 
 //Setup function (run one time at beginning)
-function setup(){
+function setup() {
   //Make the canvas fill the whole screen
   createCanvas(windowWidth, windowHeight);
 
   //Fill the [ numbers[] ] array random numbers from 0 to [ listLimit ] of length [ listLength ]
-  for(let i = 0; i < listLength; i++){
+  for (let i = 0; i < listLength; i++) {
     numbers[i] = Math.round(Math.random() * listLimit);
   }
 
@@ -26,7 +26,7 @@ function setup(){
 }
 
 //Function continually run
-function draw(){
+function draw() {
   //Clear canvas each frame
   clear();
 
@@ -35,11 +35,11 @@ function draw(){
   noStroke();
 
   //Create the bars array at the length of [ listLength ] and with the heights of from the [ numbers[] ] array
-  for(let i = 0; i < listLength; i++){
+  for (let i = 0; i < listLength; i++) {
     let height = numbers[i];
-    bars[i] = rect((xunit * i), listLimit*yunit, xunit, yunit*-height);
+    bars[i] = rect((xunit * i), listLimit * yunit, xunit, yunit * -height);
   }
-   //throw new Error("Script finished! - I threw an error to stop this from eating up your CPU.")
+  //throw new Error("Script finished! - I threw an error to stop this from eating up your CPU.")
 }
 
 //Run every time window is resized
@@ -51,26 +51,25 @@ function windowResized() {
 }
 
 
-function customSort(array){
-  let p = round((array.length-1) /2);
+function customSort(array) {
+  let p = round((array.length - 1) / 2);
   let pvalue = array[p];
   let bigger = [];
   let smaller = [];
 
-
-    for (let i = 0; i < array.length; i++){
+  for (let i = 0; i < array.length; i++) {
     let current = array[i];
-      if (current <= pvalue){
-        smaller.push(current);
-      } else {
-        bigger.push(current);
-      }
+    if (current <= pvalue) {
+      smaller.push(current);
+    } else {
+      bigger.push(current);
     }
+  }
 
   iteration++;
-  if (iteration < 100){
-     smaller = customSort(smaller);
-     bigger = customSort(bigger);
+  if (iteration < 100) {
+    smaller = customSort(smaller);
+    bigger = customSort(bigger);
   }
 
   let newArray = smaller.concat(bigger);
